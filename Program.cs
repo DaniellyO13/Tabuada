b794ea92cffb2db1b7eb799c9b3e6eac3fd2a671
Danielly Oliveira <danyoliveiraa001@gmail.com>
2025-10-31 20:48:55 -0300

    Conclusão

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("--- Tábuada ---");
+
+int n = 1;
+int numeroDigitado;
+
+Console.Write("Digite um Número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+
+
+while (n <= 10)
+{
+    Console.WriteLine($"{numeroDigitado} X {n} = {numeroDigitado * n}");
+    n = n + 1;
+}
